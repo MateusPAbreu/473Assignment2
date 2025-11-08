09027e3d234be237354654550f20160a825def4a
Josh <joshholuboch@gmail.com>
2025-11-07 21:53:39 -0800

    Just refactoring for structure
    We had 2 mains, we now have the 1 correct main

--- Assignment2/FPgrowth.py
+++ Assignment2/FPgrowth.py
@@ -1,67 +1,40 @@
 import sys
-from database import Database #unclear why database import is not working
-# JO: on my end this import works fine
+from database import Database
 from FPtree import FPtree
-from node import Node
 import time
 
-file_name = sys.argv[1] #this is the file path we will be opening
-sup = int(sys.argv[2]) #minimum support
-sup_percent = sup/100
-min_sup = 0
-data:Database
-
-
-
+file_name = sys.argv[1]
+sup = int(sys.argv[2])
+sup_percent = sup / 100
 
 def main():
     data = Database(file_name)
-    global min_sup
-    min_sup = int(data.size * sup_percent) #amount of items * sup_percent -> MST
-
-    start_time= time.time()
-
-    # JO: testing the changes I made
+    min_sup = int(data.size * sup_percent)
+    
+    start_time = time.time()
+    
+    # Build FP-tree
     fp_tree = FPtree()
-    fp_tree.build_table(data, min_sup) # must do this before the build_tree call
+    fp_tree.build_table(data, min_sup)
     fp_tree.build_tree(data)
-    print(fp_tree) # this is to test if things are working, also depends on if I overrode __str__ properly
-
     
-    for key in reversed(fp_tree.header_table):
-        print("Building projected tree for itemset " + str(set(key)))
-        link = fp_tree.header_table[key][1]
-        tree_header = fp_tree.build_projected_table(link)
-        print("    Header table is " + str(tree_header))
-      #  proj_tree = FPtree(tree_root, tree_header) # MO i dont need this because the projected tree method is changed
-
-    proj_tree = fp_tree.build_projected_tree(link, min_sup) # MO: since the projected tree makes a new tree instea dof passing it through main now i chnaged the aprameters
-    print(proj_tree)
-    stuff=frozenset()
-    result= fp_tree.mining(stuff, min_sup)
-    print("The frequent patterns are : "+str( result ))
-
-    end_time= time.time()
-    elapsed_time= end_time-start_time
-
-    file= open("MiningResult_"+ file_name, "w")
-    file.write(f" |FPs| = {len(result)}\n")
-    for elem in result:
-        file.write(f"{result.index(elem)} : {elem}\n")
-
-    file.close
-
-
-
+    # Mine patterns
+    result = fp_tree.mining(frozenset(), min_sup)
     
-    # When I run this, nothing is printed, but no errors. Might have made a mistake in overriding __str__, 
-    # could just check by adding print statements in build_table and build_tree, but I am busy for tonight
-    # so I'll have to leave it to you guys.
-
-
-    #in all honesty, I am unclear in how we are supposed to find which items are connected to other items without making more scans of the database
-    # JO: We can do it by following the header table hyperlinks, to build the smaller trees, then we just need to store
-    # the frequent patterns, and find a way to do it recursively.
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    
+    
+    print(f"minsup = {sup}% = {min_sup}")
+    print(f"|FPs| = {len(result)}")
+    print(f"Total Runtime: {elapsed_time:.3f} sec")
+    
+   
+    output_file = f"MiningResult_{file_name}"
+    with open(output_file, "w") as file:
+        file.write(f"|FPs| = {len(result)}\n")
+        for pattern, support in result:
+            file.write(f"{set(pattern)} : {support}\n")
 
 if __name__ == "__main__":
     main()
